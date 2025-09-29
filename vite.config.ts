0c443f44512859d34828474e08da4d382f4d2ab4
powhwee <powhwee@gmail.com>
2025-09-29 15:04:59 +0800

    removed gemini_api_key

--- vite.config.ts
+++ vite.config.ts
@@ -9,16 +9,12 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
-	https: true,
+        https: true,
       },
       plugins: [
       	       react(),
       	       basicSsl()
 	       ],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
